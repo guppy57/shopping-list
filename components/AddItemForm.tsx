bb60b6d3d04d28e1532a5cb4edd17a5131aba5b0
Armaan Gupta <armaan@guppy.im>
2023-04-19 15:21:10 -0500

    feat: setup add item form

---
+++ components/AddItemForm.tsx
@@ -0,0 +1,42 @@
+export default function AddItemForm(): JSX.Element {
+  return (
+    <div className={"mt-4"}>
+      <form className={"flex flex-row"}>
+        <div className={"flex-grow mr-2"}>
+          <label className={"text-sm text-gray-500"} htmlFor="description">
+            Description
+          </label>
+          <input
+            type="text"
+            name="description"
+            id="description"
+            className="block bg-green-50 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-800 sm:text-sm sm:leading-6"
+            placeholder="What item do you need?"
+          />
+        </div>
+        <div>
+          <label className={"text-sm text-gray-500"} htmlFor="quantity">
+            Qty
+          </label>
+          <input
+            type="number"
+            name="quantity"
+            id="quantity"
+            className="block w-16 bg-green-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-800 sm:text-sm sm:leading-6"
+            placeholder="3"
+          />
+        </div>
+        <div className={"h-full self-end"}>
+          <button
+            className={
+              "bg-green-500 hover:bg-green-400 text-white font-medium py-1.5 px-4 rounded ml-2 shadow-md"
+            }
+            type={"submit"}
+          >
+            Add item
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
