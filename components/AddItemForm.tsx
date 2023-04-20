277c10c4dce048764490a15055078aff36e45454
Armaan Gupta <armaan@guppy.im>
2023-04-20 13:44:33 -0500

    ref: using state variables

--- components/AddItemForm.tsx
+++ components/AddItemForm.tsx
@@ -1,7 +1,45 @@
+import { useAtom } from "jotai";
+import shoppingListStore from "../stores/shoppingList.store";
+import React from "react";
+
+const generateId = (length: number): string => {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+};
+
 export default function AddItemForm(): JSX.Element {
+  const [shoppingList, setShoppingList] = useAtom(shoppingListStore);
+  const [description, setDescription] = React.useState("");
+  const [quantity, setQuantity] = React.useState(1);
+
+  const addNewItem = (event: React.FormEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    if (description === "") {
+      return;
+    }
+
+    const newItem = {
+      id: generateId(15),
+      description,
+      quantity: parseInt(quantity.toString()),
+      purchased: false,
+    };
+
+    setShoppingList([...shoppingList, newItem]);
+  };
+
   return (
-    <div className={"mt-4"}>
-      <form className={"flex flex-row"}>
+    <div className={"mt-4 sticky"}>
+      <div className={"flex flex-row"}>
         <div className={"flex-grow mr-2"}>
           <label className={"text-sm text-gray-500"} htmlFor="description">
             Description
@@ -12,6 +50,8 @@ export default function AddItemForm(): JSX.Element {
             id="description"
             className="block bg-green-50 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-800 sm:text-sm sm:leading-6"
             placeholder="What item do you need?"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           />
         </div>
         <div>
@@ -24,6 +64,8 @@ export default function AddItemForm(): JSX.Element {
             id="quantity"
             className="block w-16 bg-green-50 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-800 sm:text-sm sm:leading-6"
             placeholder="3"
+            value={quantity}
+            onChange={(e) => setQuantity(parseInt(e.target.value))}
           />
         </div>
         <div className={"h-full self-end"}>
@@ -31,12 +73,13 @@ export default function AddItemForm(): JSX.Element {
             className={
               "bg-green-500 hover:bg-green-400 text-white font-medium py-1.5 px-4 rounded ml-2 shadow-md"
             }
-            type={"submit"}
+            type={"button"}
+            onClick={addNewItem}
           >
             Add item
           </button>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
