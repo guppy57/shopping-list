785af82f309a4b539aae0e700c00e2ae5afaf772
Armaan Gupta <armaan@guppy.im>
2023-05-30 13:11:21 -0500

    test: wrote Header tests

--- __tests__/components/Header.test.js
+++ __tests__/components/Header.test.js
@@ -3,4 +3,23 @@ import { render, screen } from "@testing-library/react";
 import Header from "../../components/Header";
 
 describe("Header - Dumb Component", () => {
+  it("renders correct headline", () => {
+    render(<Header />);
+    const heading = screen.getByRole("heading", {
+      name: /Guppy's Shopping List/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("has correct profile picture", () => {
+    render(<Header />);
+    const profilePicture = screen.getByAltText("Guppy profile picture");
+    expect(profilePicture.src).toContain('guppy.jpg');
+  });
+
+  it("should have correct link", () => {
+    render(<Header />);
+    const link = screen.getByRole("link");
+    expect(link.href).toBe("https://my.kreativeusa.com/");
+  })
 });
