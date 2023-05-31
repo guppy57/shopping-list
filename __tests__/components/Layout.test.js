6ddb79442c8487a9b5887f3b6a694200de51ceff
Armaan Gupta <armaan@guppy.im>
2023-05-31 21:09:14 +0000

    test: created tests for Layout component

--- __tests__/components/Layout.test.js
+++ __tests__/components/Layout.test.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Header from "../../components/Layout";
+import Layout from "../../components/Layout";
 
 describe("Layout - Dumb Component", () => {
+    it("should render main component", () => {
+        render(<Layout/>);
+        const main = screen.getByRole("main");
+        expect(main).toBeInTheDocument();
+    });
 });
