7d5b5f24230f11d9651fef7d2b143b3f4591d120
Armaan Gupta <armaan@guppy.im>
2023-06-01 18:36:58 +0000

    test: created tests for generateId

--- __tests__/lib/generateId.test.jsx
+++ __tests__/lib/generateId.test.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Header from "../../lib/generateId";
+import generateId from "../../lib/generateId";
 
 describe("generateId - Library", () => {
+    it("should fail from invalid prop", () => {
+        try {
+            generateId("lol");
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+            expect(error.message).toBe('Invalid action');
+        }
+    });
+
+    it("should return new id as string with correct length", () => {
+        const id = generateId(6);
+        expect(id.length).toBe(6);
+        expect(typeof id).toBe('string');
+    });
 });
