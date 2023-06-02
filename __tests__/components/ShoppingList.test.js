df794f050bc550f7b8e0d67c984d29b7663d2aab
Armaan Gupta <armaan@guppy.im>
2023-06-02 11:56:02 -0500

    test: wrote ShoppingList tests

--- __tests__/components/ShoppingList.test.js
+++ __tests__/components/ShoppingList.test.js
@@ -1,6 +1,18 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Header from "../../components/ShoppingList";
+import ShoppingList from "../../components/ShoppingList";
 
 describe("ShoppingList - Dumb Component", () => {
+  it("should have the correct number of list items", () => {
+      render(<ShoppingList />);
+      const listItems = screen.getAllByRole("checkbox");
+      expect(listItems.length).toEqual(4);
+  });
+
+  it("should render correct type of children", () => {
+    render(<ShoppingList />);
+    const ItemsHolder = screen.getByTestId("items-holder");
+    const listItems = ItemsHolder.children;
+    expect(listItems[0]).toBeInstanceOf(HTMLDivElement);
+  });
 });
