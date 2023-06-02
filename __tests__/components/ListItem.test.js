50d7202966f3068de38de93a4e3c79b521b7e196
Armaan Gupta <armaan@guppy.im>
2023-06-02 12:29:21 -0500

    test: wrote basic tests for ListItem

--- __tests__/components/ListItem.test.js
+++ __tests__/components/ListItem.test.js
@@ -1,6 +1,33 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Header from "../../components/ListItem";
+import ListItem from "../../components/ListItem";
+
+const exampleListItem = {
+  id: "9h98y732bpodsfd",
+  description: "Bread",
+  quantity: 2,
+  purchased: false,
+}
 
 describe("List Item - Dumb Component", () => {
+  it("should have a checkbox", () => {
+    render(<ListItem
+      indexId={exampleListItem.id}
+      description={exampleListItem.description}
+      quantity={exampleListItem.quantity}
+      purchased={exampleListItem.purchased}
+    />);
+    expect(screen.getByRole("checkbox")).toBeInTheDocument();
+  });
+
+  it("should render props correctly", () => {
+    render(<ListItem
+      indexId={exampleListItem.id}
+      description={exampleListItem.description}
+      quantity={exampleListItem.quantity}
+      purchased={exampleListItem.purchased}
+    />);
+    expect(screen.getByText(exampleListItem.description)).toBeInTheDocument();
+    expect(screen.getByText(exampleListItem.quantity)).toBeInTheDocument();
+  });
 });
