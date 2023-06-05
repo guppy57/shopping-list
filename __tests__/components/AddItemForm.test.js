f565eb9ef6605d186d5e0fea99248f6f2147cfe1
Armaan Gupta <armaan@guppy.im>
2023-06-05 15:28:30 -0500

    test: completed AddItemForm tests

--- __tests__/components/AddItemForm.test.js
+++ __tests__/components/AddItemForm.test.js
@@ -1,6 +1,48 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Header from "../../components/AddItemForm";
+import AddItemForm from "../../components/AddItemForm";
+
+const exampleShoppingList = [{
+  id: "9h98y732bpodsfd",
+  description: "Bread",
+  quantity: 2,
+  purchased: false,
+}];
+
+const addNewItem = (description, quanity) => {
+  exampleShoppingList.push({
+    id: "9h98y732bpodsfd",
+    description: description,
+    quantity: quanity,
+    purchased: false,
+  });
+};
 
 describe("AddItemForm - Smart Component", () => {
+  beforeEach(() => {
+    render(<AddItemForm addNewItem={addNewItem} />);
+  });
+
+  it("should have a description input", () => {
+    expect(screen.getByLabelText("Description")).toBeInTheDocument();
+  });
+
+  it("should have a quantity input", () => {
+    expect(screen.getByLabelText("Qty")).toBeInTheDocument();
+  });
+
+  it("should have a submit button", () => {
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("should handle form submit correctly", () => {
+    const descriptionInput = screen.getByLabelText("Description");
+    const quantityInput = screen.getByLabelText("Qty");
+    const submitButton = screen.getByRole("button");
+    expect(exampleShoppingList.length).toBe(1);
+    descriptionInput.value = "Milk";
+    quantityInput.value = 1;
+    submitButton.click();
+    expect(exampleShoppingList.length).toBe(2);
+  });
 });
