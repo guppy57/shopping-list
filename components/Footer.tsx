5220747fdfb4b26c6a9d62139127306066d67b36
Armaan Gupta <armaan@guppy.im>
2023-06-13 20:49:11 +0000

    ref: added button for testing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
 const Footer: React.FC = (): JSX.Element => {
+  const [title, setTitle] = useState("My Shopping List");
+
+  const buttonClick = () => {
+    if (title === "Rules suck") {
+      setTitle("My Shopping List");
+    } else {
+      setTitle("Rules suck");
+    }
+  }
+
   return (
     <footer className="bg-green-100 py-4 -mt-16">
       <div className="max-w-2xl container mx-auto flex flex-wrap items-center justify-between">
         <div className="w-full md:w-auto text-center md:text-left">
-          <span className="text-gray-700 font-bold text-lg tracking-wide opacity-50">
-            My Shopping List
+          <span data-testid="footer-title" className="text-gray-700 font-bold text-lg tracking-wide opacity-50">
+            {title}
           </span>
+          <div>
+            <button className="p-4 mb-3" onClick={buttonClick}>Click me</button>
+          </div>
         </div>
         <div className="w-full md:w-auto text-center md:text-right mt-4 md:mt-0 opacity-70">
           <a
